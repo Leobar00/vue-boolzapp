dd5514a98ca8b07742ce467d6549a0019c103684
Leonardo Barbieri <barbierileon@gmail.com>
2020-11-20 15:45:24 +0100

    top-bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
+const app = new Vue({
+    el:'#root',
+    data:{
+        counter:0,
+        contatti:[
+            {
+                avatar:'img/avatar_1.jpg',
+                nome:'Michele'
 
+            },
+            {
+                avatar: 'img/avatar_2.jpg',
+                nome: 'Leonardo',
+            },
+            {
+                avatar: 'img/avatar_3.jpg',
+                nome: 'Filippo',
+            },
+            {
+                avatar: 'img/avatar_4.jpg',
+                nome: 'Francesco',
+            },
+            {
+                avatar: 'img/avatar_5.jpg',
+                nome: 'Sandro',
+            },
+            {
+                avatar: 'img/avatar_6.jpg',
+                nome: 'Sara',
+            },
+            {
+                avatar: 'img/avatar_7.jpg',
+                nome: 'Leone',
+            }
+        ]
+    }
+})
 
 
 
